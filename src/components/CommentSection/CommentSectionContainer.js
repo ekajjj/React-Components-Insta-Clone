6c40b5f4a52bfe16799efbad5a0d5464cbeae394
Jake Johnson <jakejjohnson@gmail.com>
2020-01-23 03:18:44 -0600

    insta clone

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -5,12 +5,12 @@ import Comment from "./Comment";
 import "./Comment.css";
 
 const CommentSection = props => {
-  // Add state for the comments
-
+  const [comments]= useState(props.comments)
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
-      <CommentInput />
+       {comments.map((comment, index) => <Comment key={index} comment={comment} />)}
+      <CommentInput/>
     </div>
   );
 };
